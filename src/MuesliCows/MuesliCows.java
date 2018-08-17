d2e374114ed70b64197dea44809aee6b2c49cdd8
Josh <joshmeulman@gmail.com>
2018-08-17 21:21:07 +1000

    added attackingUs logic

--- src/MuesliCows/MuesliCows.java
+++ src/MuesliCows/MuesliCows.java
@@ -9,12 +9,12 @@ import org.rspeer.script.task.Task;
 import org.rspeer.script.task.TaskScript;
 
 
-@ScriptMeta(name = "Muesli Cows",  desc = "Kills cows in Lumbridge", developer = "Muesli", category = ScriptCategory.COMBAT)
+@ScriptMeta(name = "Muesli Cows", version = 1.1, desc = "Kills cows in Lumbridge", developer = "Muesli", category = ScriptCategory.COMBAT)
 
 public class MuesliCows extends TaskScript {
 
     private static final Task[] TASKS = { new Attack(), new Banking(), new Eat(), new Looting(), new Traverse() };
-    
+
     public static final Area COWS_AREA = Area.rectangular(new Position(3247, 3296, 0), new Position(3265, 3255, 0));
     public static final Area BANK_AREA = Area.rectangular(new Position(3208, 3220, 2), new Position(3210, 3218, 2));
 
